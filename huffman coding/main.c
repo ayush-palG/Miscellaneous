0033ceaf7d5a25d2265a38e9e37f7ab36962041f
ayush-palG <ayushpal.9470@gmail.com>
2025-02-04 13:45:57 +0530

    Introduce a sorting function to sort frequencies and filter out frequencies having count > 0

--- huffman coding/main.c
+++ huffman coding/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define CHAR_SIZE 256
+
 typedef struct {
   char ch;
   int count;
@@ -15,12 +17,67 @@ int get_text_length(const char* text)
 
 Frequency* init_frequency(void)
 {
-  Frequency* freq = (Frequency*) malloc(256 * sizeof(Frequency));
+  Frequency* freq = (Frequency*) malloc(CHAR_SIZE * sizeof(Frequency));
 
-  for (size_t i = 0; i < 256; ++i) {
+  for (size_t i = 0; i < CHAR_SIZE; ++i) {
     freq[i].ch = (char) i;
     freq[i].count = 0;
   }
+
+  return freq;
+}
+
+int get_frequency_length(const Frequency* freq)
+{
+  int counter = 0;
+
+  for (size_t i = 0; i < CHAR_SIZE; ++i) {
+    if (freq[i].count > 0) counter++;
+  }
+
+  return counter;
+}
+
+void print_frequency(const Frequency* freq)
+{
+  int freq_size = get_frequency_length(freq);
+  
+  for (size_t i = 0; i < freq_size; ++i) {
+    if (freq[i].count >= 0) {
+      printf("%d (%c) -> %d\n", freq[i].ch, freq[i].ch, freq[i].count);
+    }
+  }
+  printf("\n");
+}
+
+void ssort_frequency(Frequency* freq)
+{
+  int freq_size = get_frequency_length(freq);
+  
+  for (size_t i = 0; i < freq_size; ++i) {
+    int min_index = i;
+    
+    for (size_t j = i+1; j < freq_size; ++j) {
+      if (freq[j].count < freq[min_index].count) min_index = j;
+    }
+    
+    Frequency temp_freq = freq[i];
+    freq[i] = freq[min_index];
+    freq[min_index] = temp_freq;
+  }
+}
+
+Frequency* get_eff_frequency(Frequency* frequency)
+{
+  int freq_size = get_frequency_length(frequency);
+  Frequency* freq = (Frequency*) malloc(freq_size * sizeof(Frequency));
+
+  for (size_t i = 0, j = 0; i < CHAR_SIZE && j < freq_size; ++i) {
+    if (frequency[i].count > 0) freq[j++] = frequency[i];
+  }
+
+  free(frequency);
+  ssort_frequency(freq);
 
   return freq;
 }
@@ -34,15 +91,12 @@ Frequency* get_frequency(const char* text)
     freq[(int) text[i]].count += 1;
   }
 
+  freq = get_eff_frequency(freq);
+
   return freq;
 }
 
-void print_frequency(Frequency* freq)
 {
-  for (size_t i = 0; i < 256; ++i) {
-    if (freq[i].count > 0) {
-      printf("%zu (%c) -> %d\n", i, (char)i, freq[i].count);
-    }
   }
 }
 
@@ -55,5 +109,5 @@ void get_huffman_tree(const char* text)
 
 int main(void)
 {
-  get_huffman_tree("asdasdssss");
+  get_huffman_tree("saaaaaaaaaasdasdssss");
 }
