c862942de89dce467772b56e443e0a396203d5a9
ayush-palG <ayushpal.9470@gmail.com>
2025-02-26 19:01:37 +0530

    Implement get_digest_from_memory() to gen md5 digest from strings

--- md5/main.c
+++ md5/main.c
@@ -3,8 +3,11 @@
 
 int main(void)
 {
-  const char *digest = md5("input");
-  printf("%s\n", digest);
+  const char *file_digest = md5("input", 'f');
+  printf("Message digest from file: %s\n", file_digest);
+
+  const char *memory_digest = md5("abcdefghijklmnopqrstuvwxyz", 'm');
+  printf("Message digest from memory: %s\n", memory_digest);
   
   return 0;
 }
--- md5/stb_md5.h
+++ md5/stb_md5.h
@@ -31,24 +31,19 @@ const uint32_t key_constant[] = {0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
 			   0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
 			   0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391};
 
-uint32_t a0 = 0x67452301;
-uint32_t b0 = 0xefcdab89;
-uint32_t c0 = 0x98badcfe;
-uint32_t d0 = 0x10325476;
-
 uint32_t get_non_linear_func(uint32_t b, uint32_t c, uint32_t d, size_t i);
 size_t get_current_index(size_t i);
 uint32_t left_rotate(uint32_t func, size_t rotation_count);
-
-void swap_uint8(uint8_t *a, uint8_t *b);
-void reverse_uint8(uint8_t *arr, size_t arr_size);
 void change_endian(uint32_t *hex_input);
 
-const char *get_message_digest(FILE *file);
-const char *md5(const char *file_path);
+const char *get_message_digest_from_file(FILE *file);
+const char *get_message_digest_from_memory(const char *padded_input_str, uint64_t input_size);
+const char *md5(const char *input, char mode);
 
 uint64_t get_file_size(FILE *file);
-const char *write_padded_output_file(const char *input_file_path);
+size_t get_padded_input_size(uint64_t size);
+const char *write_padded_output_to_file(const char *input_file_path);
+const char *write_padded_output_to_memory(const char *input_str);
 
 #endif // MD5_
 
@@ -87,36 +82,26 @@ uint32_t left_rotate(uint32_t func, size_t rotation_count)
   return ((func << (rotation_count & 31)) | ((func & 0xFFFFFFFF) >> (32 - (rotation_count & 31)))) & 0xFFFFFFFF;
 }
 
-void swap_uint8(uint8_t *a, uint8_t *b)
-{
-  uint8_t temp = *a;
-  *a = *b;
-  *b = temp;  
-}
-
-void reverse_uint8(uint8_t *arr, size_t arr_size)
-{
-  for (size_t i = 0; i < arr_size / 2; ++i) {
-    swap_uint8(&arr[i], &arr[arr_size-i-1]);
-  }
-}
-
 void change_endian(uint32_t *hex_input)
 {
-  uint8_t arr[4] = {(*hex_input >> 8*0) & 0xFF,
-		    (*hex_input >> 8*1) & 0xFF,
+  uint8_t arr[4] = {(*hex_input >> 8*3) & 0xFF,
 		    (*hex_input >> 8*2) & 0xFF,
-		    (*hex_input >> 8*3) & 0xFF};
-  reverse_uint8(arr, 4);
+		    (*hex_input >> 8*1) & 0xFF,
+		    (*hex_input >> 8*0) & 0xFF};
+
   memcpy(hex_input, arr, 4);
 }
 
-const char *get_message_digest(FILE *file)
-{
-  
+const char *get_message_digest_from_file(FILE *file)
+{  
   uint64_t file_size = get_file_size(file);
   int32_t data[16];
- 
+
+  uint32_t a0 = 0x67452301;
+  uint32_t b0 = 0xefcdab89;
+  uint32_t c0 = 0x98badcfe;
+  uint32_t d0 = 0x10325476;
+  
   while ((uint64_t) ftell(file) != file_size) {
     fseek(file, 0, SEEK_CUR);
     fread(&data, sizeof(data), 1, file);
@@ -153,17 +138,71 @@ const char *get_message_digest(FILE *file)
   return digest_str;
 }
 
-const char *md5(const char *file_path)
+const char *get_message_digest_from_memory(const char *padded_input_str, uint64_t input_size)
+{ 
+  int32_t data[16];
+  size_t index = 0;
+
+  uint32_t a0 = 0x67452301;
+  uint32_t b0 = 0xefcdab89;
+  uint32_t c0 = 0x98badcfe;
+  uint32_t d0 = 0x10325476;
+ 
+  while ((uint64_t) index != input_size) {
+    memcpy(&data, &padded_input_str[index], 64);
+    index += 64;
+    
+    uint32_t a = a0;
+    uint32_t b = b0;
+    uint32_t c = c0;
+    uint32_t d = d0;
+
+    for (size_t i = 0; i < 64; ++i) {
+      uint32_t func = get_non_linear_func(b, c, d, i);
+      size_t index = get_current_index(i);
+
+      func += a + key_constant[i] + data[index];
+      a = d;
+      d = c;
+      c = b;
+      b += left_rotate(func, shift_amount[i]);
+    }
+
+    a0 += a;
+    b0 += b;
+    c0 += c;
+    d0 += d;
+  }
+
+  change_endian(&a0);
+  change_endian(&b0);
+  change_endian(&c0);
+  change_endian(&d0);
+
+  char *digest_str = (char *) malloc(sizeof(char) * DIGEST_SIZE * 2);
+  sprintf(digest_str, "%x%x%x%x", a0, b0, c0, d0);
+  return digest_str;
+}
+
+const char *md5(const char *input, char mode)
 {
-  const char *input_file_path = write_padded_output_file(file_path);
-  FILE *file = fopen(input_file_path, "rb");
-  if (file == NULL) {
-    fprintf(stderr, "ERROR: could not open output file %s: %s\n", input_file_path, strerror(errno));
-    exit(1);
+  const char *digest;
+  
+  if (mode == 'f') {
+    const char *input_file_path = write_padded_output_to_file(input);
+    FILE *file = fopen(input_file_path, "rb");
+    if (file == NULL) {
+      fprintf(stderr, "ERROR: could not open output file %s: %s\n", input_file_path, strerror(errno));
+      exit(1);
+    }
+  
+    digest = get_message_digest_from_file(file);
+  } else if (mode == 'm') {
+    const char *input_str = write_padded_output_to_memory(input);
+    uint64_t str_length = get_padded_input_size(strlen(input));
+    digest = get_message_digest_from_memory(input_str, str_length);
   }
   
-  const char *digest = get_message_digest(file);
-
   return digest;
 }
 
@@ -182,8 +221,16 @@ uint64_t get_file_size(FILE *file)
 
   return file_size;
 }
+ 
+uint64_t get_padded_input_size(uint64_t size)
+{
+  uint64_t padding_size = 64 - (size + 8) % 64;
+  if (padding_size == 0) padding_size += 64;
 
-const char *write_padded_output_file(const char *input_file_path)
+  return size + padding_size + 8;
+}
+
+const char *write_padded_output_to_file(const char *input_file_path)
 {
   FILE *input_file = fopen(input_file_path, "rb");
   if (input_file == NULL) {
@@ -220,6 +267,26 @@ const char *write_padded_output_file(const char *input_file_path)
   fclose(output_file);
 
   return output_file_path;
+}
+
+const char *write_padded_output_to_memory(const char *input_str)
+{
+  uint64_t str_length = strlen(input_str);
+  uint64_t padded_input_size = get_padded_input_size(str_length);
+  char *padded_input_str = (char *) malloc(sizeof(char) * padded_input_size);
+
+  memcpy(padded_input_str, input_str, str_length);
+  int8_t padding_value = 0x80;
+  for (size_t i = 0; i < (padded_input_size - str_length - 8); ++i) {
+    padded_input_str[str_length+i] = (char) padding_value;
+    padding_value = 0x00;
+  }
+
+  str_length *= 8;
+  memcpy(&padded_input_str[padded_input_size-8], &str_length, 8);
+
+  // for (size_t i = 0; i < padded_input_size; ++i) printf("%zu: %d\n", i, (uint8_t) padded_input_str[i]);
+  return padded_input_str;
 }
 
 #endif // MD5_IMPLEMENTATION
